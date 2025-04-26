ac059e9404a4ed0b8d1748391546871a6ab4edfb
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-04-27 05:41:38 +0800

    74. Introduction to Angular services: FF = clean up

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { Product } from './shared/models/products';
 import { ShopService } from './core/services/shop.service';
-import { Pagination } from './shared/models/pagination';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -16,23 +14,13 @@ export class AppComponent implements OnInit {
   /* implement shop service */
   private shopService = inject(ShopService);
 
-  /* refactor the code for service method
-  baseUrl = 'https://localhost:5001/api/'
-  private http = inject(HttpClient);
-  */
   title = 'Skinet';
   products: Product[] = [];
 
   ngOnInit():void {
-
-    // this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-
     this.shopService.getProducts().subscribe({
-      // return service from shop.service.ts to fix the error
       next: response => this.products = response.data,
-      error: error => console.log(error),
-
-      // complete: () => console.log('Complete')
+      error: error => console.log(error)
     })
   }
 }
