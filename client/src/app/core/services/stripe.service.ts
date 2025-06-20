8b7095c43811d6e9ec88b173bd2edf67267ccfb3
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-06-21 05:16:44 +0800

    165. Adding client side Stripe: Corrections

--- client/src/app/core/services/stripe.service.ts
+++ client/src/app/core/services/stripe.service.ts
@@ -57,7 +57,7 @@ export class StripeService {
   createOrUpdatePaymentIntent(){
     const cart = this.cartService.cart();
     if (!cart) throw new Error('Problem with cart');
-    return this.http.post<Cart>(this.baseUrl + 'payments', cart.id, {}).pipe(
+    return this.http.post<Cart>(this.baseUrl + 'payments/', cart.id, {}).pipe(
       map(cart => {
         this.cartService.cart.set(cart);
         return cart;
